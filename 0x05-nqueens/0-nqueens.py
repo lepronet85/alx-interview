ef9b3bcdd3378af824f15ee1796ec672078a371a
lepronet <lepronet85@gmail.com>
2024-08-01 16:05:53 +0100

    Update file

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,58 @@
-#!/usr/bin/python3
-""" N queens """
+#!/usr/bin/env python3
 import sys
 
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+def print_usage_and_exit():
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
-if not sys.argv[1].isdigit():
+def print_number_error_and_exit():
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
-if int(sys.argv[1]) < 4:
+def print_size_error_and_exit():
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
-n = int(sys.argv[1])
+def is_valid(board, row, col):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
 
+def solve_nqueens(N):
+    def backtrack(row):
+        if row == N:
+            solution = [[i, board[i]] for i in range(N)]
+            solutions.append(solution)
+        else:
+            for col in range(N):
+                if is_valid(board, row, col):
+                    board[row] = col
+                    backtrack(row + 1)
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+    solutions = []
+    board = [-1] * N
+    backtrack(0)
+    return solutions
 
+def main():
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print_number_error_and_exit()
 
+    if N < 4:
+        print_size_error_and_exit()
 
-solve(n)
+    solutions = solve_nqueens(N)
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    main()
+
