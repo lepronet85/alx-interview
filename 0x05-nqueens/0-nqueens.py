4d5eccff263411f38c035698b674732ac961b405
lepronet <lepronet85@gmail.com>
2024-08-01 16:10:29 +0100

    Edit file

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,84 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+N queens
+"""
+
+""" N queens """
 import sys
 
-def print_usage_and_exit():
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+try:
+    n_q = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    sys.exit(1)
+    exit(1)
 
-def print_number_error_and_exit():
+if n_q < 4:
+    print('N must be at least 4')
+if not sys.argv[1].isdigit():
     print("N must be a number")
-    sys.exit(1)
+    exit(1)
 
-def print_size_error_and_exit():
+if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    sys.exit(1)
+    exit(1)
 
-def is_valid(board, row, col):
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
+def solve_nqueens(n):
+    '''self descriptive'''
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
+
+n = int(sys.argv[1])
+
+def attack_queen(square, queen):
+    '''self descriptive'''
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+def safe_queen(sqr, queens):
+    '''self descriptive'''
+    for queen in queens:
+        if attack_queen(sqr, queen):
             return False
     return True
 
-def solve_nqueens(N):
-    def backtrack(row):
-        if row == N:
-            solution = [[i, board[i]] for i in range(N)]
-            solutions.append(solution)
-        else:
-            for col in range(N):
-                if is_valid(board, row, col):
-                    board[row] = col
-                    backtrack(row + 1)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
-    solutions = []
-    board = [-1] * N
-    backtrack(0)
-    return solutions
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
 
-def main():
-    if len(sys.argv) != 2:
-        print_usage_and_exit()
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print_number_error_and_exit()
-
-    if N < 4:
-        print_size_error_and_exit()
-
-    solutions = solve_nqueens(N)
-    for solution in solutions:
-        print(solution)
-
-if __name__ == "__main__":
-    main()
-
+solve(n)
